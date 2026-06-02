6867d74dfbc40af4d8e8bb344c8a4a6f2117939c
arianapinto-cyber <ariana.pinto@ucsp.edu.pe>
2026-06-02 16:38:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function mostrarInfo(){
+
+    document.getElementById("info").style.display = "block";
+
+    document.getElementById("relaciones").style.display = "none";
+}
+
+function mostrarRelaciones(){
+
+    document.getElementById("relaciones").style.display = "block";
+
+    document.getElementById("info").style.display = "none";
+}
